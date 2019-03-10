1c9a807b3008b908a46d6d7812b3c852f5292237
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-03-10 22:51:42 +0530

    Update Read_me.cmd

--- Assignment-2/Echo_Engine/Read_me.cmd
+++ Assignment-2/Echo_Engine/Read_me.cmd
@@ -1,1 +1,10 @@
 Instructions to run:
+
+Server:
+
+gcc -o server server.c -lpthread
+./server
+Client:
+
+gcc -o c1 c1.c // Similarly create executables for other clients
+./c1  // When program runs on single system, run other executables too
