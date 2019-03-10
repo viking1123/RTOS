57879769e80bae1d669d0a3910752067cf1dd267
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-03-10 22:45:08 +0530

    Update Read_me.cmd

--- Assignment-2/Database_Engine/Read_me.cmd
+++ Assignment-2/Database_Engine/Read_me.cmd
@@ -1,1 +1,9 @@
 Instructions to run:
+
+Server:
+1. gcc -o server server.c -lpthread
+2. ./server
+
+Client:
+1. gcc -o c1 c1.c             // Similarly create executables for other clients
+2. ./c1 127.0.0.1             //when program run on single system, run other executables too
