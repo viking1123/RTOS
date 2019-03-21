4ffab8b93222e8375c79687bbb94d0b91a6487ef
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-03-21 16:18:06 +0530

    Update client.c

--- mid_exam/openBook/client.c
+++ mid_exam/openBook/client.c
@@ -18,12 +18,11 @@ int main(){
 	char buffer[1024];
 
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket < 0)
-	{
+	if(clientSocket < 0){
 		printf("Error in connection.\n");
 		exit(1);
 	}
-	printf("Client Socket is created.\n");
+	printf(" Socket created.\n");
 
 	memset(&serverAddr, '\0', sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
@@ -31,17 +30,39 @@ int main(){
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
 	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0)
-	{
-		printf("Connection Error..\n");
+	if(ret < 0){
+		printf("Error in connection.\n");
 		exit(1);
 	}
-	printf("Connected to Server.\n");		
+	printf("Connected \n");
 		
-	n=recv(clientSocket, buffer, sizeof(buffer), 0);
+		
+n=recv(clientSocket, buffer, sizeof(buffer), 0);
 
-	buffer[n]='\0';
-	printf("Server: \t%s\n", buffer);
+					buffer[n]='\0';
+			printf("Server: \t%s\n", buffer);
 	
+
+
+
+
+FILE *fp;
+
+
+/*
+fp=fopen("parameters.csv","w+");
+fprintf(fp,"parameter1,parameter2,parameter3,parameter4,parameter5 \n");
+fclose(fp);
+*/
+
+fp=fopen("parameters.csv","a+");
+
+
+fprintf(fp,buffer);
+
+fprintf(fp,"\n");
+fclose(fp);
+	
+
 	return 0;
 }
