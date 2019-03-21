4c47cacb5cba0a98e06aafd395c01f4fd85c83f7
Sarvesh Nandkar <subtle.sarvesh95@gmail.com>
2019-03-21 10:45:17 +0530

    Update and rename mid_exam/Q1/client.c to mid_exam/openBook/client.c

---
+++ mid_exam/openBook/client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 4444
+
+int main(){
+	//clock_t t;
+	int n;
+	int clientSocket, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(clientSocket < 0)
+	{
+		printf("Error in connection.\n");
+		exit(1);
+	}
+	printf("Client Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0)
+	{
+		printf("Connection Error..\n");
+		exit(1);
+	}
+	printf("Connected to Server.\n");		
+		
+	n=recv(clientSocket, buffer, sizeof(buffer), 0);
+
+	buffer[n]='\0';
+	printf("Server: \t%s\n", buffer);
+	
+	return 0;
+}
